fcf2954100fe7c43ca2d77661752ad212c243b59
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2025-09-11 11:47:05 -0700

    Update config/env.ts
    Co-authored-by: Félix C. Morency <1102868+fmorency@users.noreply.github.com>

--- config/env.ts
+++ config/env.ts
@@ -53,7 +53,7 @@ const env = {
    * Custom proposal blockheight. This is the number of blocks above the current height that a proposal must be.
    * By default, it is set to 1000 blocks.
    */
-  upgradeMinBlockheight: parseInt(process.env.NEXT_PUBLIC_UPGRADE_MIN_BLOCKHEIGHT ?? '1000', 10),
+  upgradeMinBlockOffset: parseInt(process.env.NEXT_PUBLIC_UPGRADE_MIN_BLOCK_OFFSET ?? '1000', 10),
 };
 
 export default env;
