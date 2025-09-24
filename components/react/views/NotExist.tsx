383d6baf83a050e7c139645185a91ea994928687
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-09-24 12:44:55 -0400

    chore: switch to leap metamask snap

--- components/react/views/NotExist.tsx
+++ components/react/views/NotExist.tsx
@@ -31,7 +31,7 @@ export const NotExist = ({
           <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
         </button>
         <Dialog.Title as="h3" className="text-lg font-semibold">
-          {name === 'Cosmos MetaMask Extension' ? 'Metamask' : name}
+          {name === 'Leap Cosmos MetaMask' ? 'Metamask' : name}
         </Dialog.Title>
         <button
           type="button"
@@ -43,25 +43,23 @@ export const NotExist = ({
       </div>
       <div className="flex flex-col w-full h-full py-6 mt-4 sm:px-8">
         <img
-          src={
-            name === 'Cosmos MetaMask Extension' ? '/metamask.svg' : getRealLogo(logo, isDarkMode)
-          }
+          src={name === 'Leap Cosmos MetaMask' ? '/metamask.svg' : getRealLogo(logo, isDarkMode)}
           alt={name}
           className="shrink-0 w-16 h-16 mx-auto aspect-1"
         />
         <p className="mt-3 font-medium text-black dark:text-white">
-          Install {name === 'Cosmos MetaMask Extension' ? 'Metamask' : name}
+          Install {name === 'Leap Cosmos MetaMask' ? 'Metamask' : name}
         </p>
         <p className="mt-1 text-sm text-gray-500 dark:text-white/75">
-          To connect your {name === 'Cosmos MetaMask Extension' ? 'Metamask' : name} wallet, install
-          the browser extension.
+          To connect your {name === 'Leap Cosmos MetaMask' ? 'Metamask' : name} wallet, install the
+          browser extension.
         </p>
         <button
           className={`rounded-lg btn-primary btn btn-md ${name.length >= 12 ? 'w-1/2' : 'w-1/3'} mx-auto inline-flex justify-center items-center py-2.5 font-medium mt-4 text-white`}
           onClick={onInstall}
         >
           <ArrowDownTrayIcon className="shrink-0 w-5 h-5 mr-2 text-white" />
-          Install {name === 'Cosmos MetaMask Extension' ? 'Metamask' : name}
+          Install {name === 'Leap Cosmos MetaMask' ? 'Metamask' : name}
         </button>
       </div>
     </div>
