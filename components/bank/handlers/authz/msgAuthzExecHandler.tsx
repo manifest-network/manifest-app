7e52441a14f312d58bb99b83aade505c6f7fb8d5
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-12-19 15:44:48 -0500

    chore: improve authz exec message handler (#702)
    (cherry picked from commit b17e8476c7265eb4941b87dddfe9a01b21dd907a)

--- components/bank/handlers/authz/msgAuthzExecHandler.tsx
+++ components/bank/handlers/authz/msgAuthzExecHandler.tsx
@@ -5,9 +5,37 @@ import { createSenderReceiverHandler } from '@/components/bank/handlers/createSe
 import { registerHandler } from '@/components/bank/handlers/handlerRegistry';
 import { AdminsIcon } from '@/components/icons/AdminsIcon';
 
-const createMessage = (template: string, len: number) => {
-  const l = len ?? 'some';
-  const message = format(template, <span className="text-yellow-500">{l}</span>);
+interface CosmosMessage {
+  '@type'?: string;
+}
+
+const getUniqueTypeUrls = (msgs: CosmosMessage[] | undefined): string[] => {
+  if (!msgs) return [];
+  const typeUrls = new Set(
+    msgs.map(msg => msg['@type']).filter((type): type is string => type !== undefined)
+  );
+  return Array.from(typeUrls);
+};
+
+const createMessage = (
+  singleTypeTemplate: string,
+  multiTypeTemplate: string,
+  msgs: CosmosMessage[] | undefined
+) => {
+  const len = msgs?.length ?? 'some';
+  const uniqueTypeUrls = getUniqueTypeUrls(msgs);
+
+  if (uniqueTypeUrls.length === 1 && uniqueTypeUrls[0]) {
+    const typeUrl = uniqueTypeUrls[0];
+    const message = format(
+      singleTypeTemplate,
+      <span className="text-yellow-500">{len}</span>,
+      <span className="text-yellow-500">{typeUrl}</span>
+    );
+    return <span className="flex flex-wrap gap-1">{message}</span>;
+  }
+
+  const message = format(multiTypeTemplate, <span className="text-yellow-500">{len}</span>);
   return <span className="flex flex-wrap gap-1">{message}</span>;
 };
 
@@ -15,16 +43,22 @@ export const MsgAuthzExecHandler = createSenderReceiverHandler({
   iconSender: AdminsIcon,
   successSender: tx =>
     createMessage(
+      'You executed {0} message(s) of type {1} on behalf of another account',
       'You executed {0} message(s) on behalf of another account',
-      tx.metadata?.msgs?.length
+      tx.metadata?.msgs
     ),
   failSender: tx =>
     createMessage(
+      'You failed to execute {0} message(s) of type {1} on behalf of another account',
       'You failed to execute {0} message(s) on behalf of another account',
-      tx.metadata?.msgs?.length
+      tx.metadata?.msgs
     ),
   successReceiver: tx =>
-    createMessage('{0} message(s) were executed on your behalf', tx.metadata?.msgs?.length),
+    createMessage(
+      '{0} message(s) of type {1} were executed on your behalf',
+      '{0} message(s) were executed on your behalf',
+      tx.metadata?.msgs
+    ),
 });
 
 registerHandler(MsgExec.typeUrl, MsgAuthzExecHandler);
